d448dc230aab68aed9b7a696f64ffc58bbf4a076
dipetkov <desislavka@gmail.com>
2017-01-04 21:22:50 +0100

    Read wear sensor data (capsense) if available.

--- R/read_agd.R
+++ R/read_agd.R
@@ -59,13 +59,12 @@ read_agd <- function(file, tz = "UTC") {
 
 #' Read an *.agd file, with no post-processing
 #'
-#' Read ActiGraph sleep watch data from an SQLite database stored in an AGD file. Return a list of five tables: data, sleep, filters, settings, awakenings.
-#'
-#' These tables have the schema described in the ActiLife 6 User manual -- at least the column names, though not the data types. In particular, the timestamps are converted to human-readable POSIXct representation.
+#' Read ActiGraph sleep watch data from an SQLite database stored in an AGD file and return a list with (at least) five tables: data, sleep, filters, settings, awakenings. The tables have the schema described in the ActiLife 6 User manual and the timestamps are converted from Unix time format to human-readable POSIXct representation.
 #' @param file Full path to an agd file to read.
 #' @param tz Time zone to convert DateTime ticks to POSIX time.
-#' @return A list of five tables: settings, data, filters, sleep and awakenings.
-#' @details  In the SQLite database, timestamps are stored as ticks since 12:00:00 midnight, January 1, 0001. Each tick is one ten-millionth of a second and so ticks are of type INTEGER (long int). R does not have a 64 bit integer type and timestamps overflow. So I divide by 10,000,000 and convert to date/time with \code{STRFTIME}, \emph{before} selecting the these columns from the database.
+#' @return A list of five tables: settings, data, filters, sleep, awakenings and, if available, capsense.
+#' @details
+#' Some Actigraph devices contain a capacitive sensor to detect monitor removal when worn against the skin. If that data is available, the return list includes a capsense table as well.
 #' @references ActiLife 6 User's Manual by the ActiGraph Software Department. 04/03/2012.
 #' @references \code{covertagd}: R package for converting agd files from ActiGraph into data.frames.
 #' @seealso \code{\link{read_agd}}
@@ -83,40 +82,57 @@ read_agd_raw <- function(file, tz = "UTC") {
   # Connect to the *.agd database
   db <- src_sqlite(file)
 
-  # Get the name of all tables
-  tbl(db, sql("SELECT name FROM sqlite_master WHERE type = 'table'"))
-  # Get the CREATE statements and so the schemas
-  tbl(db, sql("SELECT sql FROM sqlite_master WHERE type = 'table'"))
-  # Cast DateTime.Ticks to POSIXct date/time
+  # Get the names of all tables in the database
+  query <- "SELECT name FROM sqlite_master WHERE type = 'table'"
+  tables_agd <- db %>% tbl(sql(query)) %>% collect() %>% .$name
+  tables_required <- c("data", "sleep", "awakenings", "filters", "settings")
+  stopifnot(all(tables_required %in% tables_agd))
+
+  # Cast Unix time ticks to POSIXct date/time
+  # Timestamps are stored as ticks since 12:00:00 midnight, January 1, 0001.
+  # Each tick is one ten-millionth of a second and so ticks are of type INTEGER
+  # (long int). R does not have a 64 bit integer type and timestamps overflow.
+  # So divide by 10,000,000 and convert to date/time with STRFTIME, before
+  # selecting these columns from the database.
   cast_dttms <- function(x) {
     if (length(x)) ymd_hms(x, tz = tz) else as.POSIXct(x)
   }
   select_dttms <- function(table, vars) {
     query <- "SELECT"
     for (var in vars) {
+      # Start counting seconds from January 1st, 1970;
       # 62135596800 is the number of seconds elapsed
       # from 01/01/0001 00:00:00 to 01/01/1970 00:00:00
-      # I.e., DateTime(1970, 1, 1, 0, 0, 0).Ticks/10000000
       query <- paste0(query,
                       " STRFTIME('%Y-%m-%d %H:%M:%S', ",
                       var, "/", "10000000 - 62135596800, ",
                       "'unixepoch') AS ", var, "_ts, ")
     }
     query <- paste0(query, " * FROM ", table)
-    tbl(db, sql(query)) %>%
+    db %>% tbl(sql(query)) %>%
       collect(n = Inf) %>%
       select(- one_of(vars)) %>%
       rename_(.dots = setNames(paste0(vars, "_ts"), vars)) %>%
       mutate_each(funs(cast_dttms), one_of(vars))
   }
+
   data <- select_dttms("data", "dataTimestamp")
-  awakenings <- select_dttms("awakenings", "timestamp")
   sleep <- select_dttms("sleep", c("inBedTimestamp",
                                    "outBedTimestamp"))
+  awakenings <- select_dttms("awakenings", "timestamp")
   filters <- select_dttms("filters", c("filterStartTimestamp",
                                        "filterStopTimestamp"))
-  settings <- tbl(db, sql("SELECT * FROM settings")) %>% collect()
+  settings <- db %>% tbl("settings") %>% collect()
+
+  # The capsense table stores data from an optional wear sensor,
+  # so it might not be present in the database.
+
+  if ("capsense" %in% tables_agd)
+    capsense <- select_dttms("capsense", "timeStamp")
+  else
+    capsense <- NULL
 
   return(list(data = data, sleep = sleep, filters = filters,
-              settings = settings, awakenings = awakenings))
+              settings = settings, awakenings = awakenings,
+              capsense = capsense))
 }
