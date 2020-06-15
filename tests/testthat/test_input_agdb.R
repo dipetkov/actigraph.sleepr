643d5899c7638ecfb39ecac78cf4fab6136af3d3
muschellij2 <muschellij2@gmail.com>
2020-06-15 11:48:19 -0400

    trying to fix the linting

--- tests/testthat/test_input_agdb.R
+++ tests/testthat/test_input_agdb.R
@@ -7,7 +7,25 @@ testthat::context("General tests")
 if (requireNamespace("lintr", quietly = TRUE)) {
   testthat::context("lints")
   testthat::test_that("Package Style", {
-    lintr::expect_lint_free()
+
+    # from https://github.com/jimhester/lintr/issues/421
+    # A duplicate copy of the find_package function from lintr
+    find_package <- function(path) {
+      path <- normalizePath(path, mustWork = FALSE)
+
+      while (!file.exists(file.path(path, "DESCRIPTION"))) {
+        path <- dirname(path)
+        if (identical(path, dirname(path))) {
+          return(NULL)
+        }
+      }
+
+      path
+    }
+
+    if (!is.null(find_package("."))) {
+      lintr::expect_lint_free()
+    }
   })
 }
 
